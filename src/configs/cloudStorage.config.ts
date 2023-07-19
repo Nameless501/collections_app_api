fbc89903a1b02e8f165d5d19bf1884fbceb47976
Eugene Zorin <ezorin5@yandex.ru>
2023-07-19 16:37:30 +0400

    cloud storage config refactored: read keys from env variables

--- src/configs/cloudStorage.config.ts
+++ src/configs/cloudStorage.config.ts
@@ -1,16 +1,36 @@
-import { fileURLToPath } from 'node:url';
-
 import 'dotenv/config.js';
 
 import { Storage } from '@google-cloud/storage';
 
+const {
+    CS_BUCKET_NAME,
+    CS_PROJECT_ID,
+    CS_KEY_ID,
+    CS_KEY,
+    CS_EMAIL,
+    CS_CLIENT_ID,
+    CS_CLIENT_CERT_URI,
+} = process.env;
+
+const config = {
+    type: 'service_account',
+    project_id: CS_PROJECT_ID,
+    private_key_id: CS_KEY_ID,
+    private_key: (CS_KEY as string).replace(/\\n/g, '\n'),
+    client_email: CS_EMAIL,
+    client_id: CS_CLIENT_ID,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: CS_CLIENT_CERT_URI,
+    universe_domain: 'googleapis.com',
+};
+
 const cloudStorage = new Storage({
-    keyFilename:
-        fileURLToPath(new URL('.', import.meta.url)) +
-        '../../cloudStorageKey.json',
-    projectId: process.env.CLOUD_PROJECT_ID,
+    projectId: CS_PROJECT_ID,
+    credentials: config,
 });
 
 export const collectionImagesBucket = cloudStorage.bucket(
-    process.env.CLOUD_BUCKET_NAME as string
+    CS_BUCKET_NAME as string
 );
